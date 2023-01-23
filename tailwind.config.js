7c208e1e0b15514428daaa0ded69bfe3320b05c8
topurayhan007 <topurayhan007@gmail.com>
2023-01-23 12:15:40 +0600

    Tweaked the banner rating div

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,14 @@ module.exports = {
   },
   plugins: [],
 };
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+};
